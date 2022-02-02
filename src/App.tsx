04a4f561aaeb42d5dfeccb61934386d0322093c1
nana <nykim@nykim.net>
2022-02-02 18:12:25 +0900

    lecture#3.4~3.6 State, Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import Circle from './Circle';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 function App() {
+  const [name, setName] = useState('');
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value }
+    } = e;
+    setName(value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`Hello, ${name}!`);
+  };
+
+  const Main = styled.div`
+    color: ${(props) => props.theme.textColor};
+    background-color: ${(props) => props.theme.bgColor};
+  `;
+
   return (
-    <>
-      <Circle borderColor="crimson" bgColor="lightsalmon" />
-      <Circle bgColor="lightgreen" text="Hello" />
-    </>
+    <Main>
+      <h1>What's your name?</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={name} onChange={onChange} />
+        <button>확인</button>
+      </form>
+    </Main>
   );
 }
 
